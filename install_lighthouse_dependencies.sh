985c85366cec2f1e7780da5dae94a022e6cf4350
int88 <golden-miner@qq.com>
2023-05-04 16:20:34 +0800

    update install_lighthouse_dependencies.sh

--- install_lighthouse_dependencies.sh
+++ install_lighthouse_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get update
+
 sudo apt install -y git gcc g++ make cmake pkg-config llvm-dev libclang-dev clang protobuf-compiler
 
 curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
@@ -7,3 +9,5 @@ curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt-get install -y nodejs
 
 npm install ganache --global
+
+apt-get install libpq-dev
